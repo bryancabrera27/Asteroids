5806c04b24cd97c2c559be1e35d6aaa60c4b641b
bryancabrera27 <71649383+bryancabrera27@users.noreply.github.com>
2020-11-17 22:36:54 -0400

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <cassert>
+#include <iostream>
+#include "App.hpp"
+
+const int WIDTH = 1136;
+const int HEIGHT = 640;
+
+int main(int argc, char* argv[])
+{
+	// Create Game Object
+	//
+	Engine::App* app = new Engine::App("Asteroids!", WIDTH, HEIGHT);
+
+	// Initialize game
+	//
+	if(!app->Init())
+	{
+		std::cout << "App Init error!\n";
+		return -1;
+	}
+
+	// Execute game
+	//
+	app->Execute();
+
+	// Delete game object
+	//
+	delete app;
+
+	return 0;
+}
